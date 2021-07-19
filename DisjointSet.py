e5ae4122f66ff3a5e18fb4c2163ef49c7f39ab70
kaustav bhattacharya <kaustavofficial1808@gmail.com>
2021-07-19 22:12:04 +0530

    Add cyclic nature of a graph using disjoint sets.

--- DisjointSet.py
+++ DisjointSet.py
@@ -1,4 +1,7 @@
 class DisjointSet:
+    element = 0
+    sets = []
+    size = []
     def __init__(self,element) -> None:
         self.element = element
         self.sets = [i for i in range(self.element)]
@@ -12,11 +15,16 @@ class DisjointSet:
     def union(self,a,b):
         root_a = self.root(a)
         root_b = self.root(b)
+        if root_a == root_b: 
+            raise Exception("Cycle forming could not merge {a} and {b}".format(a=a,b=b))
         self.sets[root_a] = root_b
 
     def weightedUnion(self,a,b):
         root_a = self.root(a)    
         root_b = self.root(b)
+        if root_a == root_b:
+            raise Exception("Cycle forming could not merge {a} and {b}".format(a=a,b=b))
+
         if (self.size[root_a] < self.size[root_b]):
             self.sets[root_a] = root_b    
             self.size[root_a] += self.size[root_b]
@@ -41,16 +49,22 @@ if __name__== '__main__':
     print("find {a} and {b} is {c}".format(a=0,b=4,c=dsjSet.find(0,4)))           
     print("find {a} and {b} is {c}".format(a=6,b=4,c=dsjSet.find(6,4)))
 
-    dsjSet2 = DisjointSet(10)
-    dsjSet2.weightedUnion(0,1)           
-    dsjSet2.weightedUnion(1,2)           
-    dsjSet2.weightedUnion(3,4)           
-    dsjSet2.weightedUnion(1,4)           
-    dsjSet2.weightedUnion(5,7)
+    dsjSet3 = DisjointSet(10)
+    dsjSet3.weightedUnion(0,1)           
+    dsjSet3.weightedUnion(1,2)           
+    dsjSet3.weightedUnion(3,4)           
+    dsjSet3.weightedUnion(1,4)           
+    dsjSet3.weightedUnion(5,7)
 
 
-    print("find {a} and {b} is {c}".format(a=1,b=4,c=dsjSet2.find(1,4)))           
-    print("find {a} and {b} is {c}".format(a=0,b=4,c=dsjSet2.find(0,4)))           
-    print("find {a} and {b} is {c}".format(a=6,b=4,c=dsjSet2.find(6,4)))           
+    print("find {a} and {b} is {c}".format(a=1,b=4,c=dsjSet3.find(1,4)))           
+    print("find {a} and {b} is {c}".format(a=0,b=4,c=dsjSet3.find(0,4)))           
+    print("find {a} and {b} is {c}".format(a=6,b=4,c=dsjSet3.find(6,4)))
+
+    dsjSet3 = DisjointSet(4)
+    dsjSet3.weightedUnion(0,1)           
+    dsjSet3.weightedUnion(2,1)           
+    dsjSet3.weightedUnion(0,2)           
+    dsjSet3.weightedUnion(1,3)            
 
 
